e9c48914b0e683be79b2fb3dc095c0502ad4ee39
wipegup <wipegup@gmail.com>
2019-04-15 15:12:40 -0600

    Binding Data

--- app/assets/javascripts/clicks.js
+++ app/assets/javascripts/clicks.js
@@ -22,16 +22,18 @@ function error() {
 function drawCircle(data){
   d3.select('#circle')    // select element with id = 'circle'
   .append('circle')     // create a circle element at that selector
+  .data(data)
   .attr("cx", 50)       // define the x-location of the circle center
   .attr("cy", 50)       // define the y-location of the circle center
-  .attr("r", data[0]);  // define the radius of the circle
+  .attr("r", function(d){return d});  // define the radius of the circle
 }
 
 function redrawCircle(data){
   d3.select('#circle')    // select element with id = 'circle'
     .select("circle")     // select the 'circle' element
+    .data(data)
     .transition()         // specify a transition should occur
-    .attr("r", data[0]);  // specify which attributes should change
+    .attr("r", function(d){return d});  // specify which attributes should change
 }
 
 // load data upon page load
